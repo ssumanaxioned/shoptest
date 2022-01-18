b2896debde0cac8f2d18943f5885d6281ceb726d
Suman028 <sumans@axioned.com>
2022-01-18 14:52:50 +0530

    fixed item component test

--- src/Component/Item/Item.test.js
+++ src/Component/Item/Item.test.js
@@ -15,11 +15,11 @@ describe('item component', ()=> {
   test('item render properly', ()=> {
     const item1 = {id: 1, title: "Clothes", image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg', price: 240}
     const item2 = {id: 2, title: "Electronics", image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg', price: 200}
-    render(<Item item={item1} />)
+    const {rerender} = render(<Item item={item1} handleClick={clickFn} />)
 
     expect(screen.getByText('Clothes')).toBeInTheDocument();
 
-    screen.rerender(<Item item={item2} />)
+    rerender(<Item item={item2} handleClick={clickFn} />)
     expect(screen.getByText('Electronics')).toBeInTheDocument();
   })
 
